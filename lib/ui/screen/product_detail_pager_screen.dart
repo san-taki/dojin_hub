4b96da27cc216744b28463731952198d76349bfc
santaki <santaki@santaki>
2021-06-17 00:20:26 +0900

    Pagerをstatelessに

--- lib/ui/screen/product_detail_pager_screen.dart
+++ lib/ui/screen/product_detail_pager_screen.dart
@@ -7,19 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ProductDetailPagerScreen extends StatefulHookWidget
-    implements ScreenType {
-  @override
-  _State createState() {
-    return _State();
-  }
-}
-
-// AutomaticKeepAliveClientMixin :
-// Stateを保持するようにマークして、Stateが破棄されないようにする
-class _State extends State<ProductDetailPagerScreen>
-    with AutomaticKeepAliveClientMixin {
-
+class ProductDetailPagerScreen extends HookWidget implements ScreenType {
   @override
   Widget build(BuildContext context) {
     var screenModel = useProvider(productDetailPagerScreenModelProvider);
@@ -41,7 +29,4 @@ class _State extends State<ProductDetailPagerScreen>
       ),
     );
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
